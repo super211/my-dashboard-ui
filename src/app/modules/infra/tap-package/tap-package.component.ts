db8a0cb0da2c6de3d752c3080f9a7aaf40cadd75
devakthk <devakthk@gmail.com>
2018-02-20 04:50:40 +0800

    Updated the infra/package to be similar with infra/deploy

--- src/app/modules/infra/tap-package/tap-package.component.ts
+++ src/app/modules/infra/tap-package/tap-package.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TCIBProvisionModel } from '../../../models/index';
+import { InfraSettings } from '../infra.settings';
 
 @Component({
   selector: 'app-tap-package',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tap-package.component.css']
 })
 export class InfraTapPackageComponent implements OnInit {
+  public tcibProvisionModel: TCIBProvisionModel;
+  public componentNames;
+  public branchNames;
 
   constructor() { }
 
   ngOnInit() {
+    this.tcibProvisionModel = new TCIBProvisionModel();
+    this.componentNames = InfraSettings.componentNames;
   }
 
+  loadBranchNames(event: any){
+    this.branchNames = InfraSettings.branchName.get(event.target.value);
+    console.log(this.branchNames);
+  }
+
+  doSubmit(){
+    
+  }
 }
